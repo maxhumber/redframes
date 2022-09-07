f5410a3ed72b870e9b11169cd9aa950a391c335c
Max Humber <max.humber@gmail.com>
2022-09-07 11:56:16 -0400

    put it all back

---
+++ TODO.md
@@ -0,0 +1,71 @@
+### todo
+
+- intrinsic
+    - ✅ `__init__`
+    - ✅ `__getitem__`
+    - ✅ `_repr_html_`
+    - ✅ `__repr__`
+    - ✅ `__eq__`
+- properties
+    - ✅ shape
+    - ✅ types
+    - ✅ columns
+    - ✅ rows
+    - ✅ empty
+- row
+    - ✅ take (any number of rows from head or tail)
+    - 🫥 slice (any number of rows from a start index to an end)
+    - ✅ sample (any rows by count or percent)
+    - 🫥 shuffle (all rows) 
+    - ✅ sort (rows by column)
+    - ✅ filter (rows by column condition)
+    - ✅ dedupe (columns by keeping only those that are unique)
+    - ✅ denix (columns by droping rows that contain NA/Null/None)
+    - ✅ fill (with down, up, and constant options)
+    - 🫥 replace (values in columns with new row options)
+- column
+    - ✅ rename (columns with dictionary)
+    - ✅ select (columns to keep)
+    - 🫥 discard (unneeded columns)
+    - ✅ mutate (new columns with functions)
+    - ✅ split (a column into other columns)
+    - combine (multiple columns into a single column)
+- other
+    - append (another DataFrame)
+    - join / merge (another DataFrame)
+    - gather
+    - spread
+    - complete
+
+
+
+    - ⚠️ export/save/write/dump
+
+    - aggregate
+
+- groupby
+    - 
+
+unique values
+value counts
+tally?
+
+
+----
+
+- docstrings
+- add datasets
+- make sure it works with fantasy
+- machine learning
+- visualization
+- cheat sheet
+- 10 minutes
+- readme
+
+to_pandas
+from_pandas
+convert
+
+SHOULD I INSTANTIATE OFF PANDAS? CONVERT FUNCTION OUTSIDE?
+WHAT ABOUT SQL?
+Instaniate from rows/columns?
---
+++ redframes/wip.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+import itertools
+from typing import Any
+from uuid import uuid4
+
+import pandas as pd
+
+
+class DataFrame:
+
+    def join(self, rhs, columns, *, how="left", suffixes=("_lhs", "_rhs")):
+        if not isinstance(rhs, DataFrame):
+            raise TypeError("rhs is not of type DataFrame")
+        if not isinstance(columns, list):
+            raise TypeError("columns is not of type list")
+        if not how in ["left", "right", "inner", "outer"]:
+            raise TypeError("")
+        lhs, rhs = self._data, rhs._data
+        data = pd.merge(lhs, rhs, on=columns, how=how, suffixes=suffixes)
+        return DataFrame(data)
+
+    def append(self, df):
+        top, bottom = self._data, df._data
+        new = pd.concat([top, bottom])
+        return DataFrame(new)
+
+    def spread(self, column, using):
+        data = self._data.copy()
+        index = [col for col in data.columns if col not in [column, using]]
+        data = pd.pivot_table(data, index=index, columns=[column], values=[using])
+        data.columns = [col for col in data.columns.get_level_values(1)]
+        data = data.reset_index().rename_axis(None, axis=0)
+        return DataFrame(data)
+
+    def gather(self, columns, into=("variable", "value"), *, dropna=True):
+        data = self._data.copy()
+        index = [col for col in data.columns if col not in columns]
+        data = pd.melt(
+            data,
+            id_vars=index,
+            value_vars=columns,
+            var_name=into[0],
+            value_name=into[1],
+        )
+        data = data.dropna(subset="value")
+        return DataFrame(data)
+
+
+    def complete(self, columns):
+        data = self._data.copy()
+        series = [data[column] for column in columns]
+        index = pd.MultiIndex.from_tuples(itertools.product(*series), names=columns)
+        data = data.set_index(columns)
+        data = data.reindex(index)
+        data = data.reset_index()
+        return DataFrame(data)
+
+    def aggregate(self, apropos, by=None):
+        data = self._data.copy()
+        if by:
+            data = data.groupby(by)
+        data = data.agg(**apropos).reset_index()
+        return DataFrame(data)
+
+    def convert(self):
+        return PandasDataFrame(self._data)
+
+    def dump(self, path=None):
+        data = self._data.copy()
+        if not path:
+            return data.to_dict(orient="list")
+        if not path.endswith(".csv"):
+            raise TypeError
+        kwargs = {"index": False} | kwargs
+        data.to_csv(path)
+        return DataFrame(data)
+
+
+class PandasDataFrame(pd.DataFrame):
+    def convert(self):
+        return DataFrame(self)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
