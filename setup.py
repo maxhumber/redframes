ab1c5f3df47bcd451a720409ba1ab927b3a568f8
Max Humber <max.humber@gmail.com>
2022-09-07 11:38:16 -0400

    squat

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+with Path('requirements.txt').open() as f:
+    install_requires = [str(req) for req in parse_requirements(f)]
+
 setup(
     name="redframes",
-    version="0.1",
+    version="0.0",
     url="https://github.com/maxhumber/redframes",
     description="[re]ctangular[d]ata[frames]",
     long_description=long_description,
@@ -13,8 +18,8 @@ setup(
     author="Max Humber",
     author_email="max.humber@gmail.com",
     license="BSD 2",
-    classifiers=[],
     packages=find_packages(),
     python_requires=">=3.9",
-    setup_requires=["setuptools>=62.1.0"],
+    install_requires=install_requires,
+    setup_requires=["setuptools>=65.3.0"],
 )
